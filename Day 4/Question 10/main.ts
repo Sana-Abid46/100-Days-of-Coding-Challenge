e771d8eb04a15bb1b4eeaf7e90ad4c5ea4eeae3a
Sana-Abid46 <sanaabid4648@gmail.com>
2024-04-01 21:46:02 +0500

    10: Adding Comments

---
+++ Day 4/Question 10/main.ts
@@ -0,0 +1,10 @@
+// Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each. If you don’t have anything specific to write because your programs are too simple at this point, just add your name and the current date at the top of each program file. Then write one sentence describing what the program does.
+
+/* Name: Sana Abid
+  
+Data: 1/4/2024
+
+Intro: Currently, I'm studying TypeScript and exploring its functionalities.
+
+About program:
+"I've learned how to comment out lines of code in the projects I'm working on it. Excited to continue learning and diving deeper into the world of TypeScript!" */
