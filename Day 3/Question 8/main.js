26467dbeb0c539c96d2ff2456d3ae1ba533732a1
Sana-Abid46 <sanaabid4648@gmail.com>
2024-04-01 21:19:07 +0500

    8: Number Eight2

---
+++ Day 3/Question 8/main.js
@@ -0,0 +1,9 @@
+/* You should create four lines that look like this:
+
+console.log(5 + 3)
+
+Your output should simply be four lines with the number 8 appearing once on each line.*/
+console.log(6 + 2); // 8  Addition
+console.log(10 - 2); // 8 Substraction
+console.log(2 * 4); // 8  Multiplication
+console.log(16 / 2); // 8 Division
