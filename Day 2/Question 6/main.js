4d3a7f0c80c1abfdd3fa48515411997e41597292
Sana-Abid46 <sanaabid4648@gmail.com>
2024-04-01 06:42:41 +0500

    Q6 Stripping Names

---
+++ Day 2/Question 6/main.js
@@ -0,0 +1,5 @@
+// Stripping Names: Store a person’s name, and include some whitespace characters at the beginning and end of the name. Make sure you use each character combination, "\t" and "\n", at least once. Print the name once, so the whitespace around the name is displayed. Then print the name after striping the white spaces.
+var Name = "\n\t Sana Memon \n\t";
+console.log(Name);
+var after = Name.trim();
+console.log(after);
