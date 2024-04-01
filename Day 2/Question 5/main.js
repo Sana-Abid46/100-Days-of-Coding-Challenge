b321d2e9e122795db8fce99a09b65c092e72aa6a
Sana-Abid46 <sanaabid4648@gmail.com>
2024-04-01 06:18:26 +0500

    Q5 : Famous Quote2

---
+++ Day 2/Question 5/main.js
@@ -0,0 +1,4 @@
+// Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable called famous_person. Then compose your message and store it in a new variable called message. Print your message.
+var famous_person = " Albert Einstein ";
+var message = '"Learn from yesterday, live for today, hope for tomorrow." ';
+console.log(message + famous_person);
