8c22e69c26908cec3742f8e085062ea14b8364e0
Sana-Abid46 <sanaabid4648@gmail.com>
2024-04-01 21:56:01 +0500

    11: Names

---
+++ Day 4/Question 11/main.js
@@ -0,0 +1,6 @@
+// Names: Store the names of a few of your friends in a array called names. Print each person’s name by accessing each element in the list, one at a time.
+var Names = ["Maria", "Fatima", "Areesha", "Hania"];
+console.log(Names[0]);
+console.log(Names[1]);
+console.log(Names[2]);
+console.log(Names[3]);
