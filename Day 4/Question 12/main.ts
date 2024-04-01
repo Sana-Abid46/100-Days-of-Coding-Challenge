99e938be269e877978e91f0f1367acd0eb1c2cc5
Sana-Abid46 <sanaabid4648@gmail.com>
2024-04-01 22:15:18 +0500

    12: Greetings

---
+++ Day 4/Question 12/main.ts
@@ -0,0 +1,13 @@
+// Greetings: Start with the array you used in Exercise 11, but instead of just printing each person’s name, print a message to them. The text of each message should be the same, but each message should be personalized with the person’s name.
+
+let Names: string[] = ["Maria", "Fatima", "Areesha", "Hania"];
+
+let message = " I miss you a lot, When will we meet?";
+
+console.log(Names[0]  +  message);
+
+console.log(Names[1]  +  message);
+
+console.log(Names[2]  +  message);
+
+console.log(Names[3]  +  message);
