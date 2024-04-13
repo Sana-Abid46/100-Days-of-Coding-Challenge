41483af4b1e7bd18898da0ab03ceb8790011ac83
Sana-Abid46 <sanaabid4648@gmail.com>
2024-04-13 21:25:48 +0500

    22: Intentional Error

---
+++ Day 8/Question 23/main.js
@@ -0,0 +1,36 @@
+/* Conditional Tests: Write a series of conditional tests. Print a statement describing each test and your prediction for the results of each test. Your code should look something like this:
+
+let car = 'subaru';
+
+console.log("Is car == 'subaru'? I predict True.")
+
+console.log(car == 'subaru')
+
+• Look closely at your results, and make sure you understand why each line evaluates to True or False.
+
+• Create at least 10 tests. Have at least 5 tests evaluate to True and another 5 tests evaluate to False.*/
+var food = "pizza";
+var item = "paper";
+var myName = "Sana Abid";
+// TRUE
+console.log("Is food is === 'pizza'? I predict True");
+console.log(food === "pizza");
+console.log("Is item is === 'paper'? I predict True");
+console.log(item === "paper");
+console.log("Is item !== food? I predict True");
+console.log(item !== food);
+console.log("item is < food ? I predict True");
+console.log(item < food);
+console.log("Is myName is === 'Sana Abid'? I predict True");
+console.log(myName === "Sana Abid");
+// FALSE
+console.log("item is > food ? I predict False");
+console.log(item > food);
+console.log("item is === food ? I predict False");
+console.log(item === food);
+console.log("Is food is !== 'pizza'? I predict False");
+console.log(food !== "pizza");
+console.log("Is item is !== 'paper'? I predict False");
+console.log(item !== "paper");
+console.log("Is myName is === food? I predict False");
+console.log(myName === item);
