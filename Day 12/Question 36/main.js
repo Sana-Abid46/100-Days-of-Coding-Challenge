d3853147879a48b7819b8ff9709668741a89a910
Sana-Abid46 <sanaabid4648@gmail.com>
2024-04-22 20:56:26 +0500

    36: T-Shirt

---
+++ Day 12/Question 36/main.js
@@ -0,0 +1,7 @@
+// T-Shirt: Write a function called make_shirt() that accepts a size and the text of a message that should be printed on the shirt. The function should print a sentence summarizing the size of the shirt and the message printed on it. Call the function.
+function make_shirt(size, message) {
+    console.log("Size: ".concat(size, ", Message: ").concat(message));
+}
+make_shirt("Small", "I love TypeScript");
+make_shirt("Medium,", "InshaAllah Future programmer");
+make_shirt("Large,", "#Coding #programming");
---
+++ Day 12/Question 36/main.ts
@@ -0,0 +1,11 @@
+// T-Shirt: Write a function called make_shirt() that accepts a size and the text of a message that should be printed on the shirt. The function should print a sentence summarizing the size of the shirt and the message printed on it. Call the function.
+
+function make_shirt(size: string, message: string) {
+    console.log(`Size: ${size}, Message: ${message}`);
+}
+
+make_shirt("Small", "I love TypeScript");
+
+make_shirt( "Medium,", "InshaAllah Future programmer");
+
+make_shirt( "Large,", "#Coding #programming");
