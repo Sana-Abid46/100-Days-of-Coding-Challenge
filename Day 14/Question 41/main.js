37af53589a3fc0af0b7c97933ced373f72a813bd
Sana-Abid46 <sanaabid4648@gmail.com>
2024-04-24 17:37:30 +0500

    40: Album

---
+++ Day 14/Question 41/main.js
@@ -0,0 +1,7 @@
+// Magicians: Make a array of magician’s names. Pass the array to a function called show_magicians(), which prints the name of each magician in the array.
+var magician = ["Harry Houdini", "David Copperfield", "Penn Jillette", "Teller"];
+function show_magicians() {
+    console.log(magician);
+}
+;
+show_magicians();
