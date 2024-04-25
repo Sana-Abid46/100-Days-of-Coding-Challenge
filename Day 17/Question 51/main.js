0a86653c616c6546e37071af693a598741995ac4
Sana-Abid46 <sanaabid4648@gmail.com>
2024-04-25 20:10:47 +0500

    51: Refactoring to Arrow Functions

---
+++ Day 17/Question 51/main.js
@@ -0,0 +1,6 @@
+// Refactoring to Arrow Functions: Take a simple function that calculates the area of a rectangle and refactor it into an arrow function.
+function calculateArea(width, height) {
+    return width * height;
+}
+// let calculateAreaArrow = (width: number, height: number): number => width * height;
+console.log(calculateArea(5, 7));
