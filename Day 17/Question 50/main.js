ac0cba3f59a0a6fbc7cc8cfa54cdabc2abd4bb59
Sana-Abid46 <sanaabid4648@gmail.com>
2024-04-25 14:49:06 +0500

    50: Multiline Template Literals

---
+++ Day 17/Question 50/main.js
@@ -0,0 +1,3 @@
+// Multiline Template Literals: Use template literals to create a multiline string that describes your ideal day. Include at least three different activities.
+var multiLine = "My ideal day involve: \n1. Waking up early and going for a job.\n2. Spending a few hours coding on a personal project.\n3. Ending the day by using mobile.";
+console.log(multiLine);
