1354b4a7a391b9bf982301da054b9f3f1e012ecf
Sana-Abid46 <sanaabid4648@gmail.com>
2024-04-28 00:34:03 +0500

    62:  Student Template

---
+++ day-21/Question 62/main.js
@@ -0,0 +1,9 @@
+// Question 62: Making a Student Template: Create a blueprint for student information, including their name, age, and the classes they're taking, and then fill in this blueprint with an example student.
+;
+var student = {
+    name: "Sana",
+    age: 18,
+    class: "Programmer",
+};
+console.log(student.age);
+console.log(student["class"]);
