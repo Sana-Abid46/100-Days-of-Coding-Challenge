7faf2e8ec2a0d38fd7b059e241c1328e3bd3e1bc
Sana-Abid46 <sanaabid4648@gmail.com>
2024-04-29 04:20:11 +0500

    64: Combine Strings and Numbers

---
+++ day-22/Question 64/main.ts
@@ -0,0 +1,9 @@
+// Question 64: Combine Strings and Numbers: Write a function that merges a piece of text with a number. For example, if provided with "Age: " and 30, it should give back "Age: 30".
+
+// Explain & TIP: In JavaScript, the + operator can add numbers together or join strings. Mixing a string with a number changes the number into a string for the joining.
+
+function combine(text:string, num:number): string {
+    return text + num 
+};
+
+console.log(combine("Age:",20));
