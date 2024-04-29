68b2df6924a46c7116aa39f7dc9fcf4a9e678573
Sana-Abid46 <sanaabid4648@gmail.com>
2024-04-29 21:24:08 +0500

    68: Multiplying Decimals

---
+++ day-23/Question 68/main.js
@@ -0,0 +1,7 @@
+// Question 68: Multiplying Decimals: Create a function that accepts two decimal numbers and returns their product. Round the result to two decimal places.
+// Explain & TIP: When working with decimals in JavaScript, results can be unexpectedly long. Rounding can help keep numbers manageable.
+function MultiplyingDecimals(num1, num2) {
+    return Math.round((num1 * num2) * 100) / 100;
+}
+console.log(MultiplyingDecimals(0.23, 0.34));
+console.log(MultiplyingDecimals(0.1, 0.2));
