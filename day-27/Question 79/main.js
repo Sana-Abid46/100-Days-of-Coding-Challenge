c76254aa19652be21c64126937751d20da18a973
Sana-Abid46 <sanaabid4648@gmail.com>
2024-05-02 21:46:26 +0500

    79

---
+++ day-27/Question 79/main.js
@@ -0,0 +1,8 @@
+// Question 79: Creating and Accessing Object Properties:
+var car = {
+    make: "Toyota",
+    model: "Corolla",
+    year: 2022,
+};
+console.log(car.model); // 1st method
+console.log(car['year']); // 2nd method
---
+++ day-27/Question 79/main.ts
@@ -0,0 +1,13 @@
+// Question 79: Creating and Accessing Object Properties:
+
+
+let car = {
+    make: "Toyota",
+    model: "Corolla",
+    year: 2022,
+};
+
+console.log(car.model);  // 1st method
+
+console.log(car['year']); // 2nd method
+
