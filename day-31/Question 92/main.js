2c6ec6edbda21c529efbb20b0e97615a384e7591
Sana-Abid46 <sanaabid4648@gmail.com>
2024-05-03 21:20:11 +0500

    92: Array (pop Method)

---
+++ day-31/Question 92/main.js
@@ -0,0 +1,6 @@
+// Question 92: Write a function to remove the last element from an array and return the removed element.
+// Explain & TIP: The .pop() method removes the last element from an array and returns that element. This can be useful when you need to work with the removed item.
+function lastElement(str) {
+    return str.pop();
+}
+console.log(lastElement(["apple", "banana", "orange"]));
