06ed5ab569a865b2881997d21d494bc4af57a801
Sana-Abid46 <sanaabid4648@gmail.com>
2024-05-03 16:17:24 +0500

    87: Extracting a Substring

---
+++ day-29/Question 87/main.js
@@ -0,0 +1,7 @@
+// Question 87: Extracting a Substring: Demonstrate how to extract the first 10 characters from a string.
+// Explain & TIP: .substring() can be used to get a specific part of a string, by specifying the start and end positions.
+function extractingSubString(str) {
+    return str.substring(0, 5);
+}
+;
+console.log(extractingSubString("hello My Name is Sana Abid!")); // Output ( hello)
