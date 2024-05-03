372a872738316e35354d17b882c3d6d66480898e
Sana-Abid46 <sanaabid4648@gmail.com>
2024-05-03 21:07:06 +0500

    91: Array

---
+++ day-31/Question 91/main.ts
@@ -0,0 +1,13 @@
+// Question 91: Create an array of your three favorite fruits and add a new fruit to the end of the array.
+
+// Explain & TIP: Adding elements to an array can be easily done using the .push() method. It allows you to add new items to the end of an array.
+
+let fruits: string[] = ["Apple", "Mango", "Banana"];
+
+// before push
+console.log(fruits);
+
+fruits.push("Cherry");
+
+// After push
+console.log(fruits);
