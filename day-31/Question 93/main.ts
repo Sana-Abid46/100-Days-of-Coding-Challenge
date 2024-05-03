09e5817a0255e3bb65b6cf4c257407804aa04939
Sana-Abid46 <sanaabid4648@gmail.com>
2024-05-04 02:11:14 +0500

    93: Find the index

---
+++ day-31/Question 93/main.ts
@@ -0,0 +1,15 @@
+// Question 93: Find the index of "Banana" in an array of fruits and replace it with "Mango".
+
+// Explain & TIP: To replace an item in an array, find its index using .indexOf(), then use that index to set a new value with array bracket notation.
+
+
+function arrayIndex(arr: string[]) {
+    const value = arr.indexOf("banana");
+    if (value !== -1) arr[value] = "Mango"
+}
+
+let fruits = ['apple', 'banana', 'orange', 'cherry', 'watermelon'];
+
+arrayIndex(fruits);
+
+console.log(fruits);
