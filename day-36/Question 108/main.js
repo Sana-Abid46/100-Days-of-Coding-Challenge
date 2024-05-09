d5da56e6393e4d52dcb6af30ab2de561aa97d251
Sana-Abid46 <sanaabid4648@gmail.com>
2024-05-10 01:35:20 +0500

    108: Compare two strings

---
+++ day-36/Question 108/main.js
@@ -0,0 +1,6 @@
+// Question 108: Compare two strings to check if they are identical, ignoring case sensitivity.
+// Explain & TIP: To compare strings without considering their case (uppercase/lowercase), you can convert both strings to the same case before comparing.
+var myName = "SANA";
+var Name = "sana";
+console.log(myName.toLowerCase() === Name.toLowerCase());
+// output.... true
---
+++ day-36/Question 108/main.ts
@@ -0,0 +1,12 @@
+// Question 108: Compare two strings to check if they are identical, ignoring case sensitivity.
+
+// Explain & TIP: To compare strings without considering their case (uppercase/lowercase), you can convert both strings to the same case before comparing.
+
+let myName = "SANA";
+
+let Name = "sana"
+
+
+console.log(myName.toLowerCase() === Name.toLowerCase());
+
+// output.... true
