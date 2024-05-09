e6dc537130b4af2dc287097bced5e1c82c540ff5
Sana-Abid46 <sanaabid4648@gmail.com>
2024-05-09 23:42:54 +0500

    105: Dice roll

---
+++ day-35/Question 105/main.ts
@@ -0,0 +1,12 @@
+// Question 105: Simulate a dice roll using JavaScript and return a random integer between 1 and 6.
+
+// Explain & TIP: Simulating a dice roll involves randomly choosing an integer between 1 and 6. Use Math.random() along with some arithmetic to achieve this range.
+
+function diceRoll(): number {
+
+    const randomNumber = Math.floor(Math.random() * 6) + 1
+    return randomNumber;
+
+}
+
+console.log(diceRoll()); // generate a random number between 1 to 6
