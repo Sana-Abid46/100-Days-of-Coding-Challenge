20f136b218c5aaf8ac940e48448fa9d7a401656e
Sana-Abid46 <sanaabid4648@gmail.com>
2024-05-16 23:36:46 +0500

    115: switch statement

---
+++ day-39/Question 115/main.js
@@ -0,0 +1,28 @@
+// Question 115: Use a switch statement to log the days of the week based on a number (1-7).
+// Explain & TIP: A switch statement is a great way to select one of many code blocks to be executed. It's particularly useful for scenarios like mapping numbers to days of the week.
+var day = 3;
+switch (day) {
+    case 1:
+        console.log("Today is Monday");
+        break;
+    case 2:
+        console.log("Today is Tuesday");
+        break;
+    case 3:
+        console.log("Today is Wednesday");
+        break;
+    case 4:
+        console.log("Today is Thursday");
+        break;
+    case 5:
+        console.log("Today is Friday");
+        break;
+    case 6:
+        console.log("Today is Saturday");
+        break;
+    case 7:
+        console.log("Today is Sunday");
+    default:
+        console.log("Invalid");
+        break;
+}
