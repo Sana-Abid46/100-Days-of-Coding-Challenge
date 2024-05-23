204a6f630e621b270d4c9d1b4bfe3ac79170bc2a
Sana-Abid46 <sanaabid4648@gmail.com>
2024-05-24 00:47:38 +0500

    133:  JSON string

---
+++ day-45/Question 133/main.ts
@@ -0,0 +1,19 @@
+// Question 133: Write a JavaScript object and convert it into a JSON string.
+
+// Explain & TIP: JSON (JavaScript Object Notation) is a lightweight data-interchange format. It's easy for humans to read and write and for machines to parse and generate. Converting a JavaScript object into a JSON string can be done using JSON.stringify().
+
+
+let person = {
+
+    name: "Sana",
+    age: 17,
+    class: true,
+
+}
+
+let jsonObj = JSON.stringify(person);
+
+console.log(jsonObj);
+// Output
+// {"name":"Sana","age":17,"class":true}
+
