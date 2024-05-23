7278ebc2b05bafe59b576a4a43317628265a261b
Sana-Abid46 <sanaabid4648@gmail.com>
2024-05-24 00:36:48 +0500

    132: Import / Export

---
+++ day-44/Question 132/app.ts
@@ -0,0 +1,7 @@
+
+// Importing named exports:
+import  { utilOne, utilTwo } from "./utils"
+
+
+// Importing a default export
+import calculator from "./main"
---
+++ day-44/Question 132/main.ts
@@ -0,0 +1,8 @@
+// Question 132: Discuss the difference between default and named exports in JavaScript modules.
+
+// Explain & TIP: JavaScript provides two main types of exports: default and named. A module can have only one default export, but multiple named exports. This flexibility supports various use cases in module design.
+
+
+export default class calculator {
+    // class definition
+}
