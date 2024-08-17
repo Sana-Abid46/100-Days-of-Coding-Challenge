82689c63e72707da483530991332db1103c772ac
Sana-Abid46 <sanaabid4648@gmail.com>
2024-08-17 16:17:21 +0500

    Q: 136

---
+++ day-46/Question 136/main.js
@@ -0,0 +1,4 @@
+// Question 136: Use console.log() to debug and track the value of a variable inside a loop.
+for (var i = 1; i <= 5; i++) {
+    console.log("iteration ".concat(i, ", i value:, i "));
+}
