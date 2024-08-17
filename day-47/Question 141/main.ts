aa5ddbb859c7f3be63b9d06c5dd3cfdd05871f68
Sana-Abid46 <sanaabid4648@gmail.com>
2024-08-18 03:01:33 +0500

    Q: 141

---
+++ day-47/Question 141/main.ts
@@ -0,0 +1,7 @@
+async function fetchData() {
+    // let result = await fetchSomething()
+    // console.log(result);
+    
+};
+
+// await keyword pauses the excution of next work until promise settle
