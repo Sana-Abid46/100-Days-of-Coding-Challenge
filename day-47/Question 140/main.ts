bffa4c43c56621317a8bd9e568e26950d5ba629a
Sana-Abid46 <sanaabid4648@gmail.com>
2024-08-18 02:54:57 +0500

    Q: 140

---
+++ day-47/Question 140/main.ts
@@ -0,0 +1,11 @@
+// Question 140: Explain the error that occurs when trying to use a reserved word as a variable name.
+
+
+
+// let const = 123; // error here because const is reserved keyword it's not used as variable name.
+
+// function var(){
+
+// } // error because var is reserved word it's not used as function name
+
+// It's important to avoid using reserved words as identifiers to prevent these errors.
