6643d93efd9055aa7afb5152c76bd5f42b425159
Sana-Abid46 <sanaabid4648@gmail.com>
2024-08-18 02:48:56 +0500

    Q: 139

---
+++ day-47/Question 139/main.js
@@ -0,0 +1,11 @@
+// Question 139: List three reserved words in JavaScript and create a valid use case for each.
+// Reserved words
+// function, let, console log
+function home() {
+    return "hello world";
+}
+;
+console.log(home());
+var a = 123;
+console.log(a);
+// let console = 22; // error because console is reserved word.
---
+++ day-47/Question 139/main.ts
@@ -0,0 +1,17 @@
+// Question 139: List three reserved words in JavaScript and create a valid use case for each.
+
+// Reserved words
+
+// function, let, console log
+
+function home(){
+   return "hello world";
+};
+console.log(home());
+
+let a = 123;
+
+console.log(a);
+
+
+// let console = 22; // error because console is reserved word.
