111d5e0ec954d70253e003d41f0c978f2524b9b4
Sana-Abid46 <sanaabid4648@gmail.com>
2024-08-25 04:19:43 +0500

    Q: 148

---
+++ day-50/Question 148/main.js
@@ -0,0 +1,8 @@
+// uestion 148: Demonstrate the use of the setTimeout() function to execute code after a delay.
+setTimeout(function () {
+    console.log("Hello World");
+}, 3000);
+function addrr() {
+    console.log('hello world: 2');
+}
+setTimeout(addrr, 2000);
---
+++ day-50/Question 148/main.ts
@@ -0,0 +1,16 @@
+// uestion 148: Demonstrate the use of the setTimeout() function to execute code after a delay.
+
+
+// First method
+setTimeout(() => {
+    console.log("Hello World");
+    
+}, 3000);
+
+
+// Second Method
+function addrr() {
+console.log('hello world: 2')
+
+}
+setTimeout(addrr, 2000)
